7e7f79093e4d2b239ad896edeca88f36cb1f054c
Marcello Coelho <marcellogcoelho@outlook.com>
2024-10-24 19:52:50 +0000

    dev: configs para formatacao com prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 80,
+    bracketSpacing: true,
+    embeddedLanguageFormatting: "auto",
+    htmlWhitespaceSensitivity: "css",
+    semi: true,
+    singleQuote: false,
+    tabWidth: 2,
+    trailingComma: "es5",
+    useTabs: false,
+  };
